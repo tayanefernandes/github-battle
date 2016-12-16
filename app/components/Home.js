110cccda32bb49553f202283b956b199ae32c8a9
Tayane Fernandes <tayane.rmf@gmail.com>
2016-12-16 18:55:09 -0200

    Adding more routes

---
+++ app/components/Home.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function () {
+    return (
+      <div className='jumbotron col-sm-12'>
+      </div>
+    )
+  }
+});
+
+module.exports = Home;
